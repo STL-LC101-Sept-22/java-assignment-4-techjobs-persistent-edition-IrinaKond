31c5b3205cfa026bf803f04420b04b9d04e4e70d
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2023-03-24 20:38:12 -0500

    graded assignment #4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,23 @@
 -- Part 1: Test it with SQL
+CREATE TABLE `job` (
+    `id` int NOT NULL,
+    `employer` varchar(255) DEFAULT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `skills` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+         LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
